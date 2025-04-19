40948387128a653ad078345cffeb56c2732299c6
esubtelnik <subtelnik.ekaterina@gmail.com>
2025-04-19 18:30:45 +0300

    add tables

--- src/components/Scenario.tsx
+++ src/components/Scenario.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
 
 interface ScenarioProps {
     scenario?: string | null;
@@ -7,7 +8,7 @@ interface ScenarioProps {
 const Scenario: React.FC<ScenarioProps> = ({ scenario }) => {
   return (
     <div className="max-h-[66vh] p-2 m-4 w-2xl overflow-y-auto text-justify scenario">
-      {scenario ? <ReactMarkdown>{scenario}</ReactMarkdown> : <div>No scenario available</div>}
+      {scenario ? <ReactMarkdown remarkPlugins={[remarkGfm]}>{scenario}</ReactMarkdown> : <div>No scenario available</div>}
     </div>
   );
 };
