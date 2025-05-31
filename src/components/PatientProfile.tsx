262e1a524599000fe706de1cf0bc50a9cac8aa40
esubtelnik <subtelnik.ekaterina@gmail.com>
2025-05-31 14:57:03 +0300

    fix

--- src/components/PatientProfile.tsx
+++ src/components/PatientProfile.tsx
@@ -1,20 +1,20 @@
 import { FC} from "react";
 import { useFileUpload } from "../context/fileContext";
 const PatientProfile: FC = () => {
-   const { patientData, patientBufferData } = useFileUpload();
+   const { patientData } = useFileUpload();
 
 
    return (
       <div className="bg-white rounded-md p-3 flex flex-col w-full items-center">
          <div className="text-2xl font-medium mb-3    ">
-            {patientData?.name || patientBufferData?.name}
+            {patientData?.name}
          </div>
          <div className="flex flex-col items-center text-sm  text-gray-500">
             <div className="flex gap-x-2">
                <span>DOB:</span>{" "}
                <span>
                   {(() => {
-                     const date = new Date(patientData?.date_of_birth || patientBufferData?.date_of_birth || "");
+                     const date = new Date(patientData?.date_of_birth || "");
                      const day = String(date.getDate()).padStart(2, "0");
                      const month = String(date.getMonth() + 1).padStart(2, "0");
                      const year = date.getFullYear();
@@ -23,7 +23,7 @@ const PatientProfile: FC = () => {
                </span>
             </div>
             <div className="flex  gap-x-2">
-               <span>MRN:</span> <span> {patientData?.medical_card || patientBufferData?.medical_card}</span>
+               <span>MRN:</span> <span> {patientData?.medical_card}</span>
             </div>
          </div>
       </div>
